52dbc5d15026127defcf3da0ba6e28d5800eb86f
Alumno <alvaro.mongil@gmail.com>
2025-05-20 14:28:21 +0200

    primer commit con código

---
+++ examen_mayo.php
@@ -0,0 +1,19 @@
+<?php
+function procesarEmailFormulario(){
+    $email = $_POST['email']??'';
+    if ($email){
+        echo "Email '". htmlspecialchars($email) . "' recibido con éxito"
+    }else{
+        echo "Por favor, envía un email en el parámetro 'email'"
+    }
+}
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    procesarEmailFormulario();
+}
+?>
+
+<form  method="post"> 
+    <input type="email" name="email" placeholder=tu.email@ejemplo.com required>
+    <button type="submit">Enviar Email</button>
+</form>
